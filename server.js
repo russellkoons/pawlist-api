0644a91a3770506533cb55b8f57b969d02cf6da1
russellkoons <rkoons@gmail.com>
2019-07-18 10:13:58 -0400

    started server config

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+mongoose.Promise = global.Promise;
+
