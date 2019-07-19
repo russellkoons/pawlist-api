232587e2583ca526ab1f771757058d38a0062bc4
russellkoons <rkoons@gmail.com>
2019-07-19 10:51:06 -0400

    Added users model and endpoint

---
+++ models.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.methods.serialize = function() {
+  return {
+    username: this.username || ''
+  }
+}
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = { User };
