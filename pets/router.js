15be8f34d075e0d5f02f50b7e4df5572d8670689
russellkoons <rkoons@gmail.com>
2019-07-22 16:22:52 -0400

    Pet POST endpoint

--- pets/router.js
+++ pets/router.js
@@ -28,4 +28,25 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  if (!('name' in req.body)) {
+    const message = 'Missing name is request body';
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  Pet.create({
+    user: req.body.user,
+    name: req.body.name,
+    info: req.body.info,
+    vet: req.body.info,
+    pic: req.body.pic
+  })
+  .then(pet => res.status(201).json(pet.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'not again!' });
+  });
+});
+
 module.exports = { router };
