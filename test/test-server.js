c652ff4e2ec74113c2825aa775c4ba50c4b7b5ce
russellkoons <rkoons@gmail.com>
2019-07-22 09:06:12 -0400

    test setup

---
+++ test/test-server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const expect = chai.expect;
+
+const { User } = require('../models');
+const { app, runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL, JWT_SECRET } = require('../config');
+
+chai.use(chaiHttp);
+
