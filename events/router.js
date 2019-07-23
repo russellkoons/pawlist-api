9bb06f5a70cb5b3856506b49a33c4db86b4b2b8f
russellkoons <rkoons@gmail.com>
2019-07-23 13:17:05 -0400

    Event GET endpoint

--- events/router.js
+++ events/router.js
@@ -6,4 +6,16 @@ const { Event } = require('../models');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Event
+    .find()
+    .then(events => {
+      res.json(events.map(event => event.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Nooooo' });
+    });
+});
+
 module.exports = { router };
