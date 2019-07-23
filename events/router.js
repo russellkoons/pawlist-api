c542159a9a7ecad612408d43e5bc3ada26287100
russellkoons <rkoons@gmail.com>
2019-07-23 13:26:02 -0400

    Event POST endpoint

--- events/router.js
+++ events/router.js
@@ -28,6 +28,25 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  if (!('name' in req.body)) {
+    const message = 'Missing name is request body';
+    console.error(message);
+    return res.status(400).send(message);
+  }
 
+  Event.create({
+    user: req.body.user,
+    name: req.body.name,
+    info: req.body.info,
+    date: req.body.date,
+    frequency: req.body.frequency
+  })
+  .then(event => res.status(201).json(event.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'Don\'t make me come over there' });
+  });
+});
 
 module.exports = { router };
